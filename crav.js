4c2036a3f0f18dd4d285f6c135c789608a421547
DEVARAJ M <144207988+Devarajmohan@users.noreply.github.com>
2024-07-24 14:14:30 +0530

    Update crav.js

--- crav.js
+++ crav.js
@@ -1,27 +1,20 @@
-let slider = document.querySelector(".slider .list"),
-    items = document.querySelectorAll(".slider .list .item"),
-    next = document.getElementById("next"),
-    prev = document.getElementById("prev"),
-    dots = document.querySelectorAll(".slider .dots li"),
-    lengthItems = items.length - 1,
-    active = 0;
-next.onclick = function() { active = active + 1 <= lengthItems ? active + 1 : 0, reloadSlider() }, prev.onclick = function() { active = active - 1 >= 0 ? active - 1 : lengthItems, reloadSlider() };
-let refreshInterval = setInterval(() => { next.click() }, 3e3);
+"use strict";
+const addEventOnElements = function(e, t, a) { for (let l = 0; l < e.length; l++) e[l].addEventListener(t, a) },
+    [navbar, navToggler, navbarLinks] = [document.querySelector("[data-navbar]"), document.querySelector("[data-nav-toggler]"), document.querySelectorAll("[data-nav-link]")],
+    toggleNavbar = function() { navbar.classList.toggle("active"), this.classList.toggle("active"), document.body.classList.toggle("active") };
+navToggler.addEventListener("click", toggleNavbar);
+const closeNavbar = function() { navbar.classList.remove("active"), navToggler.classList.remove("active"), document.body.classList.remove("active") };
+addEventOnElements(navbarLinks, "click", closeNavbar);
+const header = document.querySelector("[data-header]"),
+    activeElemOnScroll = function() { window.scrollY >= 50 ? header.classList.add("active") : header.classList.remove("active") };
+window.addEventListener("scroll", activeElemOnScroll);
+const revealElements = document.querySelectorAll("[data-reveal]"),
+    revealOnScroll = function() { for (let e = 0; e < revealElements.length; e++) revealElements[e].getBoundingClientRect().top < window.innerHeight / 1.1 && (revealElements[e].classList.add("revealed"), revealElements[e].classList.contains("btn") && setTimeout(function() { revealElements[e].style.transition = "0.25s ease" }, 1e3)) };
+window.addEventListener("scroll", revealOnScroll), revealOnScroll(), document.addEventListener("DOMContentLoaded", e => { let t = ['url("Flat White.webp")', 'url("Latte.webp")', 'url("Macchiato.webp")'],
+        a = 0,
+        l = document.querySelector(".hero");
 
-function reloadSlider() {
-    let e = items[active].offsetWidth,
-        t = -active * e;
-    slider.style.left = t + "px";
-    document.querySelector(".slider .dots li.active").classList.remove("active"), dots[active].classList.add("active"), clearInterval(refreshInterval), refreshInterval = setInterval(() => { next.click() }, 1500)
-}
-
-function toggleMenu() {
-    let e = document.querySelector(".off-screen-menu");
-    e.classList.toggle("active")
-}
-
-function closeMenu() {
-    let e = document.querySelector(".off-screen-menu");
-    setTimeout(() => { e.classList.remove("active") }, 800)
-}
-window.onresize = function(e) { reloadSlider() }, document.querySelector(".footer").addEventListener("click", function() { window.scrollTo({ top: 0, behavior: "smooth" }) });
+    function n() { a = (a + 1) % t.length, l.style.backgroundImage = t[a] }
+    setInterval(n, 2e3) }), document.addEventListener("DOMContentLoaded", function() { let e = document.querySelectorAll("[data-reveal]"),
+        t = new IntersectionObserver((e, t) => { e.forEach(e => { e.isIntersecting && (e.target.classList.add("is-visible"), t.unobserve(e.target)) }) }, { threshold: .1 });
+    e.forEach(e => { t.observe(e) }) });
